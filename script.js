69afd831dedfea255d38d8c50262118a84bf0416
EasyFullz <protelco6655@gmail.com>
2024-06-11 14:23:48 -0500

    Trabaje con promesas utilizando then y catch en el caso de que sean en cascada y al ser de forma asincrona se utilizo async y await

---
+++ script.js
@@ -0,0 +1,49 @@
+const urlNasa = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15";
+
+//async y await
+async function listaImagenes() {
+    try{
+        let fetchImagen = await fetch(urlNasa);
+        let datosImagenes = await fetchImagen.json();
+
+        const card = document.querySelector("[data-ul]");
+
+        datosImagenes.forEach(element => {
+            const contenido = 
+
+            `<li class="card">
+                <img class="card__image" src="${element.url}" alt="imagen">
+                <h3 class="card__title">${element.title}</h3>
+            </li>`
+
+            card.innerHTML = card.innerHTML + contenido;
+        });
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
+listaImagenes();
+
+// then y catch
+/* function listaImagenes () {
+
+    fetch(urlNasa)
+    .then( response => response.json())
+    .then( datosImagenes => {
+        console.log(datosImagenes);
+
+        const card = document.querySelector("[data-ul]");
+
+        datosImagenes.forEach(element => {
+            const contenido = `<li class="card">
+                                    <img class="card__image" src="${element.url}" alt="imagen">
+                                    <h3 class="card__title">${element.title}</h3>
+                                </li>`
+            card.innerHTML = card.innerHTML + contenido;
+        });
+    })
+    .catch(error => console.log("error"));
+}
+listaImagenes(); */
